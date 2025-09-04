8eee25721402083e6a65dbf95d4b330c04a5d173
Keshav Kumar <keshavgujj99@gmail.com>
2025-09-04 09:48:14 +0530

    Day-2 Largest Element Naive Solution Added

--- DSAProject/src/array/learning/LargestElement.java
+++ DSAProject/src/array/learning/LargestElement.java
@@ -1,9 +1,11 @@
 package array.learning;
 
+import java.util.Arrays;
+
 public class LargestElement {
     public static void main(String[] args) {
-        int[] arr = {40, 8, 5, 100};
-        int largestIndex = largest(arr);
+        int[] arr = {10, 5, 20, 8};
+        int largestIndex = largestOptimal(arr);
         System.out.println("Largest Element: " + arr[largestIndex]);
     }
 
@@ -12,7 +14,7 @@ Optimal Solution:
 Time Complexity: O(n)
 Space Complexity:O(1)
 */
-    private static int largest(int[] arr) {
+    private static int largestOptimal(int[] arr) {
         int index = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > arr[index]) {
@@ -20,6 +22,16 @@ Space Complexity:O(1)
             }
         }
         return index;
+    }
+
+    /*
+Naive Solution:
+Time Complexity: O(n*logn)
+Space Complexity:O(1)
+*/
+    private static int largest(int[] arr) {
+        Arrays.sort(arr);
+        return arr.length - 1;
     }
 }
 
