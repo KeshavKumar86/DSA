569844472a386f7f19e4b885cf9f52ebb2d7c839
Keshav Kumar <keshavgujj99@gmail.com>
2025-09-06 19:35:30 +0530

    Day-4 Linear Search

---
+++ DSAProject/src/array/learning/LinearSearch.java
@@ -0,0 +1,22 @@
+package array.learning;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int num = 3;
+        System.out.println("Number found at index: " + search(arr, num));
+    }
+    /*
+Optimal Solution:
+Time Complexity: O(n)
+Space Complexity:O(1)
+*/
+    private static int search(int[] arr, int num) {
+        for (int i = 0; i < arr.length; i++) {
+            if (num == arr[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
