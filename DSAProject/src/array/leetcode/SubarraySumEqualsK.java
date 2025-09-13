aba74024c756742a65eaf585ef700d9f8810fdb5
Keshav Kumar <keshavgujj99@gmail.com>
2025-09-13 22:46:16 +0530

    Day-11 Subarray Sum Equals K

---
+++ DSAProject/src/array/leetcode/SubarraySumEqualsK.java
@@ -0,0 +1,52 @@
+package array.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        int k = 3;
+        System.out.println("Sub-Array Count: " + subarraySum(arr, k));
+    }
+
+    /*
+Optimal Solution:
+Time Complexity: O(n)
+Space Complexity:O(n)
+*/
+    private static int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int prefixSum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int num : nums) {
+            prefixSum += num;
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+        return count;
+    }
+
+    /*
+Naive Solution:
+Time Complexity: O(n^2)
+Space Complexity:O(1)
+*/
+    private static int subarraySumNaive(int[] nums, int k) {
+        int n = nums.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
