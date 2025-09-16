795143169548a5eb7638925febb04350677d3f4e
Keshav Kumar <keshavgujj99@gmail.com>
2025-09-16 08:54:47 +0530

    Day-14 3 Sum Improved

--- DSAProject/src/array/leetcode/ThreeSum.java
+++ DSAProject/src/array/leetcode/ThreeSum.java
@@ -14,13 +14,16 @@ public class ThreeSum {
     /*
 Optimal Solution:
 Time Complexity: O(n^2)
-Space Complexity:O(n)
+Space Complexity:O(1)
 */
     private static List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-        Set<List<Integer>> result = new HashSet<>();
         Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
         for (int i = 0; i < n - 2; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
             int low = i + 1;
             int high = nums.length - 1;
             while (low < high) {
@@ -33,11 +36,44 @@ Space Complexity:O(n)
                     Collections.sort(list);
                     result.add(list);
                     low++;
+                    high--;
+                    while (low < high && nums[low] == nums[low - 1]) {
+                        low++;
+                    }
+                    while (low < high && nums[high] == nums[high + 1]) {
+                        high--;
+                    }
                 } else if (tripleSum > 0) {
                     high--;
                 } else {
                     low++;
                 }
+            }
+        }
+        return result;
+    }
+
+    /*
+Better Solution:
+Time Complexity: O(n^2)
+Space Complexity:O(n)
+*/
+    private static List<List<Integer>> threeSumBetter(int[] nums) {
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        Set<List<Integer>> result = new HashSet<>();
+        for (int i = 0; i < n - 1; i++) {
+            set.clear();
+            for (int j = i + 1; j < n; j++) {
+                if (set.contains(-nums[j] - nums[i])) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(-nums[j] - nums[i]);
+                    Collections.sort(list);
+                    result.add(list);
+                }
+                set.add(nums[j]);
             }
         }
         return new ArrayList<>(result);
