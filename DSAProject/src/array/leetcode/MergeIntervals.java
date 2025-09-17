7572572940007f062df5b0e65d8939b9b87e98e3
Keshav Kumar <keshavgujj99@gmail.com>
2025-09-17 09:50:09 +0530

    Day-15 Merge Intervals

---
+++ DSAProject/src/array/leetcode/MergeIntervals.java
@@ -0,0 +1,38 @@
+package array.leetcode;
+
+import java.util.Arrays;
+
+public class MergeIntervals {
+    public static void main(String[] args) {
+        int[][] arr = new int[4][];
+        arr[0] = new int[]{1, 4};
+        arr[1] = new int[]{5, 12};
+        arr[2] = new int[]{12, 17};
+        arr[3] = new int[]{15, 18};
+        int[][] result = merge(arr);
+        for (int[] element : result) {
+            System.out.print(Arrays.toString(element) + " ");
+        }
+    }
+
+    private static int[][] merge(int[][] intervals) {
+        int n = intervals.length;
+        int[][] result = new int[n][2];
+        int index = 0;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        result[index++] = intervals[0];
+        for (int i = 1; i < n; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            //if overlapping intervals then merge them else just add current interval
+            if (result[index - 1][1] >= start) {
+                result[index - 1][1] = Math.max(result[index - 1][1], end);
+            } else {
+                result[index++] = intervals[i];
+            }
+        }
+        int[][] finalResult = new int[index][];
+        System.arraycopy(result, 0, finalResult, 0, index);
+        return finalResult;
+    }
+}
