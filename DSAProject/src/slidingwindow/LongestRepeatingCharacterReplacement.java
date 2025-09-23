1f6a3ae9e25ee2fa39060abd269446618261e547
Keshav Kumar <keshavgujj99@gmail.com>
2025-09-23 10:22:08 +0530

    Day-21 Added Naive Solution

--- DSAProject/src/slidingwindow/LongestRepeatingCharacterReplacement.java
+++ DSAProject/src/slidingwindow/LongestRepeatingCharacterReplacement.java
@@ -2,16 +2,17 @@ package slidingwindow;
 
 public class LongestRepeatingCharacterReplacement {
     public static void main(String[] args) {
-        String s = "AABABBA";
-        int k = 1;
+        String s = "ABAB";
+        int k = 2;
         System.out.println("Maximum Length Substring: " + characterReplacement(s, k));
     }
+
     /*
 Optimal Solution:
 Time Complexity: O(2*n)
 Space Complexity:O(26)
 */
-    private static int characterReplacement(String s, int k) {
+    private static int characterReplacementOptimal(String s, int k) {
         int n = s.length();
         int[] freq = new int[26];
         int start = 0, end;
@@ -26,6 +27,28 @@ Space Complexity:O(26)
                 start++;
             }
             maxLength = Math.max(maxLength, (end - start + 1));
+        }
+        return maxLength;
+    }
+    /*
+Naive Solution:
+Time Complexity: O(n^2)
+Space Complexity:O(26)
+*/
+    private static int characterReplacement(String s, int k) {
+        int n = s.length();
+        int maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            int maxFreq = 0;
+            int[] freq = new int[26];
+            for (int j = i; j < n; j++) {
+                freq[s.charAt(j) - 'A']++;
+                maxFreq = Math.max(maxFreq, freq[s.charAt(j) - 'A']);
+                if (j - i + 1 - maxFreq > k) {
+                    break;
+                }
+                maxLength = Math.max(maxLength, (j - i + 1));
+            }
         }
         return maxLength;
     }
