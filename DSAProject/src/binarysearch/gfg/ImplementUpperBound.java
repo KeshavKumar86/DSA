362158d0b38e58f7c8cc7bcdd9bde0d9b539a87e
Keshav Kumar <keshavgujj99@gmail.com>
2025-09-28 11:31:31 +0530

    Day-26 Implement Upper Bound

---
+++ DSAProject/src/binarysearch/gfg/ImplementUpperBound.java
@@ -0,0 +1,43 @@
+package binarysearch.gfg;
+
+public class ImplementUpperBound {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 7, 10, 11, 11, 11, 11, 11, 11, 14, 16, 18, 25};
+        int target = 11;
+        System.out.println("Element's Index: " + upperBound(arr, target));
+    }
+    /*
+Optimal Solution:
+Time Complexity: O(logn)
+Space Complexity:O(1)
+*/
+    private static int upperBound(int[] arr, int target) {
+        // code here
+        int n = arr.length;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] <= target) {
+                left = mid + 1;
+
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+    /*
+Naive Solution:
+Time Complexity: O(n)
+Space Complexity:O(1)
+*/
+    private static int upperBoundNaive(int[] arr, int target) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > target) {
+                return i;
+            }
+        }
+        return n;
+    }
+}
