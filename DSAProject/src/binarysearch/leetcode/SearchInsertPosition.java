9204dc760d3a2f59d5ddf7f6a9d26ddb365fd41f
Keshav Kumar <keshavgujj99@gmail.com>
2025-09-28 12:20:45 +0530

    Day-26 Search Insert Position

---
+++ DSAProject/src/binarysearch/leetcode/SearchInsertPosition.java
@@ -0,0 +1,43 @@
+package binarysearch.leetcode;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 6};
+        int target = 7;
+        System.out.println("Element's Index: " + searchInsertNaive(arr, target));
+    }
+
+    /*
+Optimal Solution:
+Time Complexity: O(logn)
+Space Complexity:O(1)
+*/
+    private static int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    /*
+Naive Solution:
+Time Complexity: O(n)
+Space Complexity:O(1)
+*/
+    private static int searchInsertNaive(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return n;
+    }
+}
