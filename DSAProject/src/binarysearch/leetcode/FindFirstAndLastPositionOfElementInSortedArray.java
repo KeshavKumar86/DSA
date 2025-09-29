224bd1ce886b51680e66bd3ddc11c7895eb39ee9
Keshav Kumar <keshavgujj99@gmail.com>
2025-09-29 09:32:38 +0530

    Day-27 Find First And Last Position Of Element In Sorted Array

---
+++ DSAProject/src/binarysearch/leetcode/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,85 @@
+package binarysearch.leetcode;
+
+import java.util.Arrays;
+
+public class FindFirstAndLastPositionOfElementInSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 7, 7, 8, 8, 10};
+        int target = 3;
+        System.out.println("First & Last Occurrence Index: " + Arrays.toString(searchRangeNaive(arr, target)));
+    }
+
+    /*
+Optimal Solution:
+Time Complexity: O(2*logn)
+Space Complexity:O(1)
+*/
+    private static int[] searchRange(int[] nums, int target) {
+        int firstIndex = firstOccurance(nums, target);
+        if (firstIndex == -1) {
+            return new int[]{-1, -1};
+        }
+        int lastIndex = lastOccurance(nums, target);
+        return new int[]{firstIndex, lastIndex};
+    }
+
+    private static int firstOccurance(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int resIndex = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                resIndex = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return resIndex;
+    }
+
+    private static int lastOccurance(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int resIndex = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                resIndex = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return resIndex;
+    }
+
+    private static int[] searchRangeNaive(int[] nums, int target) {
+        int n = nums.length;
+        int firstIndex = -1, lastIndex = -1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == target) {
+                firstIndex = i;
+                break;
+            }
+        }
+        for (int i = firstIndex + 1; i < n; i++) {
+            if (nums[i] > target) {
+                lastIndex = i - 1;
+                break;
+            }
+        }
+        if (firstIndex == -1) {
+            return new int[]{-1, -1};
+        }
+        if (lastIndex == -1) {
+            return new int[]{firstIndex, n - 1};
+        }
+        return new int[]{firstIndex, lastIndex};
+    }
+}
