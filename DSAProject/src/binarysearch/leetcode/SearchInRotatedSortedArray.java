7703e8d5fa932ac795c0c30d081b3831b09ffaad
Keshav Kumar <keshavgujj99@gmail.com>
2025-09-30 08:53:43 +0530

    Day-28 Search In Rotated Sorted Array

--- DSAProject/src/binarysearch/leetcode/SearchInRotatedSortedArray.java
+++ DSAProject/src/binarysearch/leetcode/SearchInRotatedSortedArray.java
@@ -7,6 +7,11 @@ public class SearchInRotatedSortedArray {
         System.out.println("Element Present at Index: " + search(arr, target));
     }
 
+    /*
+Optimal Solution:
+Time Complexity: O(logn)
+Space Complexity:O(1)
+*/
     private static int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
@@ -29,6 +34,20 @@ public class SearchInRotatedSortedArray {
                 } else {
                     left = mid + 1;
                 }
+            }
+        }
+        return -1;
+    }
+
+    /*
+Naive Solution: Linear Search
+Time Complexity: O(n)
+Space Complexity:O(1)
+*/
+    private static int searchNaive(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
             }
         }
         return -1;
