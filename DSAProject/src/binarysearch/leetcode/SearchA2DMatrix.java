448a9032adcf02cab1b373e6bd006a86e751e150
Keshav Kumar <keshavgujj99@gmail.com>
2025-10-13 10:11:34 +0530

    Day-40 SearchA2DMatrix

---
+++ DSAProject/src/binarysearch/leetcode/SearchA2DMatrix.java
@@ -0,0 +1,52 @@
+package binarysearch.leetcode;
+
+public class SearchA2DMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][];
+        matrix[0] = new int[]{1, 3, 5, 7};
+        matrix[1] = new int[]{10, 11, 16, 20};
+        matrix[2] = new int[]{23, 30, 34, 60};
+        int target = 12;
+        //matrix[3] = new int[]{0, 0, 0, 0};
+        System.out.println("Is " + target + " present: " + searchMatrix(matrix, target));
+    }
+    /*
+ Optimal Solution:
+ Time Complexity: O(logn + logm)
+ Space Complexity:O(1)
+ */
+    private static boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int left = 0, right = n - 1;
+        if (target < matrix[0][0] || target > matrix[n - 1][m - 1]) {
+            return false;
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[mid][m - 1] == target) {
+                return true;
+            } else if (matrix[mid][m - 1] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+
+        }
+        int row = left;
+        left = 0;
+        right = m - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+}
