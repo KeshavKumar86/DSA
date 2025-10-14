8f6b105d8f77c98629ff56168e8f12cc4d41fbaf
Keshav Kumar <keshavgujj99@gmail.com>
2025-10-14 09:45:55 +0530

    Day-41 Search A 2D MatrixII

---
+++ DSAProject/src/binarysearch/leetcode/SearchA2DMatrixII.java
@@ -0,0 +1,43 @@
+package binarysearch.leetcode;
+
+public class SearchA2DMatrixII {
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][];
+        matrix[0] = new int[]{1, 3, 5, 7};
+        matrix[1] = new int[]{10, 11, 16, 20};
+        matrix[2] = new int[]{23, 30, 34, 60};
+        int target = 12;
+        System.out.println("Is " + target + " present: " + searchMatrix(matrix, target));
+    }
+
+    /*
+ Optimal Solution:
+ Time Complexity: O(n+m)
+ Space Complexity:O(1)
+ */
+    private static boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0, col = m - 1;
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (target < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+        /*
+ Better Solution: Iterate each row and apply binary search on each row
+ Time Complexity: O(n*logm)
+ Space Complexity:O(1)
+ */
+        /*
+ Naive Solution: Iterate each element linearly and check if it is equal to target
+ Time Complexity: O(n*m)
+ Space Complexity:O(1)
+ */
+}
