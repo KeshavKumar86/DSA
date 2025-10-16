188861797791b73a741dfcf71ab752cc44d83ad7
Keshav Kumar <keshavgujj99@gmail.com>
2025-10-16 09:35:09 +0530

    Day-43 Reverse Words In A String

--- DSAProject/src/string/leetcode/ReverseWordsInAString.java
+++ DSAProject/src/string/leetcode/ReverseWordsInAString.java
@@ -2,7 +2,7 @@ package string.leetcode;
 
 public class ReverseWordsInAString {
     public static void main(String[] args) {
-        String s = "a good   example";
+        String s = "    a     good   example    ";
         System.out.println("Reverse Words String: " + reverseWordsOptimal(s));
     }
 
@@ -24,18 +24,42 @@ public class ReverseWordsInAString {
         }
         return sb.toString();
     }
-
+    /*
+ Optimal Solution:
+ Time Complexity: O(n)
+ Space Complexity:O(n)
+ */
     private static String reverseWordsOptimal(String s) {
-        s = s.trim();
-        String[] arr = s.split("\\s+");
-        int n = arr.length;
         StringBuilder sb = new StringBuilder();
-        for (int i = n - 1; i >= 0; i--) {
-            sb.append(arr[i]);
-            if (i != 0) {
-                sb.append(" ");
+        StringBuilder result = new StringBuilder();
+        int end = s.length() - 1, start = 0;
+        boolean isWord = false;
+        boolean firstWordAdded = false;
+        for (int i = end; i >= start; i--) {
+            if (s.charAt(i) != ' ') {
+                isWord = true;
+                sb.append(s.charAt(i));
+            } else {
+                if (isWord) {
+                    if (firstWordAdded) {
+                        result.append(' ');
+                    }
+                    firstWordAdded = true;
+                    result.append(sb.reverse());
+                    sb.setLength(0);
+                    isWord = false;
+                }
             }
         }
-        return sb.toString();
+        if (isWord) {
+            if (firstWordAdded) {
+                result.append(' ');
+            }
+            result.append(sb.reverse());
+        }
+        if (result.charAt(result.length() - 1) == ' ') {
+            result.deleteCharAt(result.length() - 1);
+        }
+        return result.toString();
     }
 }
