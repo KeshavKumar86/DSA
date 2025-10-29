7b86227e6b666a82e2f3c5c5670d6a2a5aa914c0
Keshav Kumar <keshavgujj99@gmail.com>
2025-10-29 10:32:41 +0530

    Day-50 Linked List Cycle

---
+++ DSAProject/src/linkedlist/leetcode/LinkedListCycle.java
@@ -0,0 +1,57 @@
+package linkedlist.leetcode;
+
+import java.util.HashSet;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode secondNode = new ListNode(2);
+        head.next = secondNode;
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = secondNode;
+        System.out.println("Has Cycle: " + hasCycle(head));
+
+    }
+
+    /*
+ Optimal Solution:
+ Time Complexity: O(n)
+ Space Complexity:O(1)
+ */
+    private static boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next == null) {
+                return false;
+            }
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*
+ Optimal Solution:
+ Time Complexity: O(n)
+ Space Complexity:O(n)
+ */
+    private static boolean hasCycleNaive(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        if (head == null)
+            return false;
+        while (head != null) {
+            if (!set.add(head))
+                return true;
+            head = head.next;
+        }
+        return false;
+    }
+}
