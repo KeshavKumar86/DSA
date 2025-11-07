4b3c2a0ab6bbef89d67a0a76a4be4dd12cf521c3
Keshav Kumar <keshavgujj99@gmail.com>
2025-11-07 07:34:47 +0530

    Day-58 Find Pairs With Given Sum In Doubly Linked List

---
+++ DSAProject/src/linkedlist/gfg/FindPairsWithGivenSumInDoublyLinkedList.java
@@ -0,0 +1,37 @@
+package linkedlist.gfg;
+
+import java.util.ArrayList;
+
+public class FindPairsWithGivenSumInDoublyLinkedList {
+    public static void main(String[] args) {
+
+    }
+    /*
+ Optimal Solution:
+ Time Complexity: O(2*n)
+ Space Complexity:O(1)
+ */
+    private static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target,
+                                                                      DoublyNode head) {
+        DoublyNode end = head;
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        while (end.next != null) {
+            end = end.next;
+        }
+        DoublyNode start = head;
+        while (start != end) {
+            if (start.data + end.data == target) {
+                ArrayList<Integer> pair = new ArrayList<>();
+                pair.add(start.data);
+                pair.add(end.data);
+                result.add(pair);
+                end = end.prev;
+            } else if (start.data + end.data > target) {
+                end = end.prev;
+            } else {
+                start = start.next;
+            }
+        }
+        return result;
+    }
+}
