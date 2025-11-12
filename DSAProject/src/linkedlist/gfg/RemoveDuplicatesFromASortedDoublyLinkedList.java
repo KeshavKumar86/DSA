8f26c9ef3492a9e989f3b82b667c1d317e8aa093
Keshav Kumar <keshavgujj99@gmail.com>
2025-11-12 08:34:34 +0530

    Day-61 Remove Duplicates From A Sorted Doubly Linked List Added Complexity

--- DSAProject/src/linkedlist/gfg/RemoveDuplicatesFromASortedDoublyLinkedList.java
+++ DSAProject/src/linkedlist/gfg/RemoveDuplicatesFromASortedDoublyLinkedList.java
@@ -4,13 +4,16 @@ public class RemoveDuplicatesFromASortedDoublyLinkedList {
     public static void main(String[] args) {
 
     }
-
+    /*
+ Optimal Solution:
+ Time Complexity: O(n)
+ Space Complexity:O(1)
+ */
     private static DoublyNode removeDuplicates(DoublyNode head) {
         DoublyNode curr = head;
         while (curr.next != null) {
             if (curr.data == curr.next.data) {
                 DoublyNode next = curr.next.next;
-                curr.next.prev = null;
                 curr.next = next;
                 if (next != null) {
                     next.prev = curr;
