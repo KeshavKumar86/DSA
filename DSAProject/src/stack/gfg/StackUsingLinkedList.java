68b8f382e8e19c03ba7b4b9b27bdf92952f8bff7
Keshav Kumar <keshavgujj99@gmail.com>
2025-11-14 10:01:21 +0530

    Day-63 Stack Using Linked List

---
+++ DSAProject/src/stack/gfg/StackUsingLinkedList.java
@@ -0,0 +1,50 @@
+package stack.gfg;
+
+import java.util.LinkedList;
+
+public class StackUsingLinkedList {
+    public static void main(String[] args) {
+
+    }
+}
+
+class myStack {
+
+    LinkedList<Integer> stack;
+
+    public myStack() {
+        stack = new LinkedList<>();
+    }
+
+    public boolean isEmpty() {
+        // check if the stack is empty
+        return stack.isEmpty();
+    }
+
+    public void push(int x) {
+        // Adds an element at the TOP of the stack (front of linked list)
+        stack.addFirst(x);
+    }
+
+    public void pop() {
+        // Removes the TOP element of the stack
+        if (!stack.isEmpty()) {
+            stack.removeFirst();
+        }
+    }
+
+    public int peek() {
+        // Returns the TOP element of the stack.
+        // If stack is empty, return -1.
+        if (stack.isEmpty()) {
+            return -1;
+        }
+        return stack.get(0);
+    }
+
+    public int size() {
+        // Returns the current size of the stack
+        return stack.size();
+    }
+}
+
