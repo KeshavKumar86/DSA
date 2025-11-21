a4623bb3bc2c8b9d1777065a7af00b1ea5464f2b
Keshav Kumar <keshavgujj99@gmail.com>
2025-11-21 09:43:29 +0530

    Day-68 Next Greater Element II Naive Solution Added

--- DSAProject/src/stack/leetcode/NextGreaterElementII.java
+++ DSAProject/src/stack/leetcode/NextGreaterElementII.java
@@ -5,9 +5,10 @@ import java.util.Stack;
 
 public class NextGreaterElementII {
     public static void main(String[] args) {
-        int[] nums = {5, 1, 4};
-        System.out.println("Next Greater Element: " + Arrays.toString(nextGreaterElements(nums)));
+        int[] nums = {1, 3, 2, 4, 6, 5, 7, 9};
+        System.out.println("Next Greater Element: " + Arrays.toString(nextGreaterElementsNaive(nums)));
     }
+
     /*
      Optimal Solution: Using monotonic stack
      Time Complexity: O(2*n)
@@ -26,6 +27,26 @@ public class NextGreaterElementII {
             }
             if (i < n) {
                 stack.push(i);
+            }
+        }
+        return res;
+    }
+
+    /*
+    Naive Solution: Brute Force Solution
+    Time Complexity: O(n^2)
+    Space Complexity: O(n)
+    */
+    private static int[] nextGreaterElementsNaive(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < i + n; j++) {
+                if (nums[j % n] > nums[i]) {
+                    res[i] = nums[j % n];
+                    break;
+                }
             }
         }
         return res;
