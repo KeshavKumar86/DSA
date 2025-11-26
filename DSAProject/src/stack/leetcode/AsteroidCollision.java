54be6f5c316d7a43abf1622c84fdd1b620334c0d
Keshav Kumar <keshavgujj99@gmail.com>
2025-11-26 09:43:23 +0530

    Day-71 Asteroid Collision

---
+++ DSAProject/src/stack/leetcode/AsteroidCollision.java
@@ -0,0 +1,42 @@
+package stack.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class AsteroidCollision {
+    public static void main(String[] args) {
+        int[] arr = {8, 8, 8, -8};
+        System.out.println("Final Result: " + Arrays.toString(asteroidCollision(arr)));
+    }
+
+        /*
+         Optimal Solution:
+         Time Complexity: O(n)
+         Space Complexity:O(n)
+       */
+    private static int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int n = asteroids.length;
+        for (int asteroid : asteroids) {
+            while (!stack.isEmpty() && stack.peek() > 0 && asteroid < 0 && stack.peek() < Math.abs(asteroid)) {
+                stack.pop();
+
+            }
+            if (!stack.isEmpty() && stack.peek() > 0 && asteroid < 0 && stack.peek() == Math.abs(asteroid)) {
+                stack.pop();
+                continue;
+            }
+            if (!stack.isEmpty() && stack.peek() > 0 && asteroid < 0 && stack.peek() > Math.abs(asteroid)) {
+                continue;
+            }
+            stack.push(asteroid);
+        }
+        int size = stack.size();
+        int[] res = new int[size];
+        for (int i = size - 1; i >= 0; i--) {
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+}
