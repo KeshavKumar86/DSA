5f377d419dd7b6836e2aaafe496baa9f4241bc20
Keshav Kumar <keshavgujj99@gmail.com>
2025-11-30 09:41:17 +0530

    Day-73 Remove K Digits

--- DSAProject/src/stack/leetcode/RemoveKDigits.java
+++ DSAProject/src/stack/leetcode/RemoveKDigits.java
@@ -5,21 +5,24 @@ import java.util.Deque;
 
 public class RemoveKDigits {
     public static void main(String[] args) {
-        String num = "1432219";
-        int k = 3;
+        String num = "1764523";
+        int k = 4;
         System.out.println("Result: " + removeKdigits(num, k));
     }
 
+    /*
+     Optimal Solution:
+     Time Complexity: O(n)
+     Space Complexity:O(n)
+    */
     private static String removeKdigits(String num, int k) {
         int n = num.length();
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             if (k > 0) {
-                if (!stack.isEmpty()) {
-                    if (stack.peek() > (num.charAt(i) - '0')) {
-                        stack.pop();
-                        k--;
-                    }
+                while (!stack.isEmpty() && stack.peek() > (num.charAt(i) - '0') && k > 0) {
+                    stack.pop();
+                    k--;
                 }
             }
             stack.push(num.charAt(i) - '0');
@@ -28,16 +31,19 @@ public class RemoveKDigits {
         while (!stack.isEmpty()) {
             res.append(stack.pop());
         }
+        // if k is not zero remove k digits
         res = new StringBuilder(res.substring(k));
         res.reverse();
         int length = res.length();
         int i;
+        //remove leading zeros
         for (i = 0; i < length; i++) {
             if (res.charAt(i) != '0') {
                 break;
             }
         }
         String result = res.substring(i);
+        //if all digits removed then return 0
         if (result.isEmpty()) {
             return "0";
         }
