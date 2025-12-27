2877908ef67ef14b81aa2c7cdb4b6fa3887fd790
Keshav Kumar <keshavgujj99@gmail.com>
2025-12-27 21:53:36 +0530

    Day-81 Improved Optimal Solution

--- DSAProject/src/binarysearch/gfg/CeilInASortedArray.java
+++ DSAProject/src/binarysearch/gfg/CeilInASortedArray.java
@@ -6,6 +6,7 @@ public class CeilInASortedArray {
         int x = 11;
         System.out.println("Ceil's Index: " + findCeil(arr, x));
     }
+
     /*
 Optimal Solution:
 Time Complexity: O(logn)
@@ -13,29 +14,29 @@ Space Complexity:O(1)
 */
     private static int findCeil(int[] arr, int x) {
         int n = arr.length;
+        int ans = -1;
         int left = 0, right = n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] < x) {
-                left = mid + 1;
-            } else {
+            if (arr[mid] >= x) {
+                ans = mid;
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        if (left == n) {
-            return -1;
-        }
-        return left;
+        return ans;
     }
+
     /*
 Naive Solution:
 Time Complexity: O(n)
 Space Complexity:O(1)
 */
-    private static  int findCeilNaive(int[] arr, int x){
+    private static int findCeilNaive(int[] arr, int x) {
         int n = arr.length;
-        for(int i=0;i<n;i++){
-            if(arr[i]>=x){
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= x) {
                 return i;
             }
         }
--- DSAProject/src/binarysearch/gfg/FloorInASortedArray.java
+++ DSAProject/src/binarysearch/gfg/FloorInASortedArray.java
@@ -14,17 +14,21 @@ Space Complexity:O(1)
 */
     private static int findFloor(int[] arr, int x) {
         int n = arr.length;
+        int ans = -1;
         int left = 0, right = n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] > x) {
-                right = mid - 1;
-            } else {
+            if (arr[mid] <= x) {
+                ans = mid;
                 left = mid + 1;
+
+            } else {
+                right = mid - 1;
             }
         }
-        return right;
+        return ans;
     }
+
     /*
 Naive Solution:
 Time Complexity: O(n)
