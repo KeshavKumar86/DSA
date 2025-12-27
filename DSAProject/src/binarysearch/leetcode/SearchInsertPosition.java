3c0532ab9a19519762bcdf6919ba16ee7f1c9f48
Keshav Kumar <keshavgujj99@gmail.com>
2025-12-27 21:30:11 +0530

    Day-81 Search Insert Position Improved Optimal Solution

--- DSAProject/src/binarysearch/leetcode/SearchInsertPosition.java
+++ DSAProject/src/binarysearch/leetcode/SearchInsertPosition.java
@@ -14,16 +14,18 @@ Space Complexity:O(1)
 */
     private static int searchInsert(int[] nums, int target) {
         int n = nums.length;
+        int ans = n;
         int left = 0, right = n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
+            if (nums[mid] >= target) {
+                ans = mid;
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        return left;
+        return ans;
     }
 
     /*
