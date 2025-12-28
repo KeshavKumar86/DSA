986835505d368220221ff8dfa2e3cdcaf74e1aee
Keshav Kumar <keshavgujj99@gmail.com>
2025-12-28 20:43:51 +0530

    Day-82 Improved Optimal Solution

--- DSAProject/src/binarysearch/leetcode/SingleElementInASortedArray.java
+++ DSAProject/src/binarysearch/leetcode/SingleElementInASortedArray.java
@@ -5,6 +5,7 @@ public class SingleElementInASortedArray {
         int[] arr = {1, 1, 2, 3, 3, 4, 4, 8, 8};
         System.out.println("Single Element: " + singleNonDuplicate(arr));
     }
+
     /*
 Optimal Solution:
 Time Complexity: O(logn)
@@ -15,25 +16,27 @@ Space Complexity:O(1)
         int left = 0, right = n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if ((mid == 0 || nums[mid - 1] != nums[mid]) && (mid == n - 1 || nums[mid] != nums[mid + 1])) {
+            // edge cases when answer is not present on the left side and right side is out of bound
+            if (mid == n - 1) {
                 return nums[mid];
             }
             if (mid % 2 == 0) {
-                if (nums[mid - 1] == nums[mid]) {
-                    right = mid - 1;
-                } else {
+                if (nums[mid] == nums[mid + 1]) {
                     left = mid + 1;
+                } else {
+                    right = mid - 1;
                 }
             } else {
-                if (nums[mid - 1] == nums[mid]) {
+                if (nums[mid] == nums[mid - 1]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
                 }
             }
         }
-        return -1;
+        return nums[left];
     }
+
     /*
 Naive Solution:
 Time Complexity: O(n)
